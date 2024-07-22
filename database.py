91e46a0982058aaef6ac2a573f6e7d102b8284e1
abubakar4360 <abubakarabbasi541@gmail.com>
2024-07-22 18:18:10 +0500

    Initial commit

--- database.py
+++ database.py
@@ -1,72 +1,9 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
-from model import Employee as EmployeeModel, Admin as AdminModel
+from sqlalchemy.orm import sessionmaker
 from model import Base
-from schemas import Employee as EmployeeSchema, AdminCreate as AdminCreateSchema
-from passlib.context import CryptContext
 
-SQLALCHEMY_DB_URL = "sqlite:///employee.db"
-engine =  create_engine(SQLALCHEMY_DB_URL, connect_args={'check_same_thread': False})
+db_url = 'sqlite:///./login.db'
+
+engine = create_engine(db_url, connect_args={'check_same_thread' : False})
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 Base.metadata.create_all(bind=engine)
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-def add_new_employee(db: Session, employee: EmployeeSchema):
-    db_new_employee = EmployeeModel(
-        id = employee.id,
-        name = employee.name,
-        email = employee.email,
-        age = employee.age,
-        salary = employee.salary,
-        role =employee.role
-    )
-    db.add(db_new_employee)
-    db.commit()
-    db.refresh(db_new_employee)
-    return db_new_employee
-
-def delete_employee(db: Session, employee_id: int):
-    db_del_employee = db.query(EmployeeModel).filter(EmployeeModel.id == employee_id).first()
-    if db_del_employee:
-        db.delete(db_del_employee)
-        db.commit()
-        return True
-    return False
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
-def add_admin(db: Session, admin: AdminCreateSchema):
-    db_admin = AdminModel(
-        username=admin.username,
-        hashed_password=get_password_hash(admin.password)
-    )
-    db.add(db_admin)
-    db.commit()
-    db.refresh(db_admin)
-    return db_admin
-
-def update_admin_password(username: str, new_password: str, db: Session):
-    admin = db.query(AdminModel).filter(AdminModel.username == username).first()
-    if admin:
-        hashed_password = get_password_hash(new_password)
-        admin.hashed_password = hashed_password
-        db.commit()
-        db.refresh(admin)
-        return admin
-    return None
-
-
-def get_admin(db: Session, username: str):
-    return db.query(AdminModel).filter(AdminModel.username == username).first()
-
-
--- model.py
+++ model.py
@@ -1,23 +1,35 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+import datetime
 
 Base = declarative_base()
 
-class Admin(Base):
-    __tablename__ = "admins"
+class UserTable(Base):
+    __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    username = Column(String(50), unique=True, nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(20), nullable=False)
+    is_verified = Column(Boolean)
 
-class Employee(Base):
-    __tablename__ = "employees"
+class EmployeeTable(Base):
+    __tablename__ = 'employee'
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    email = Column(String, unique=True, index=True)
+    id = Column(Integer, primary_key=True, unique=True)
+    name =Column(String(100), nullable=False)
     age = Column(Integer)
-    salary = Column(Integer)
-    role = Column(String)
+    email = Column(String(100), unique=True, nullable=False)
+    role = Column(String(100), nullable=False)
+    salary = Column(Integer, nullable=False)
 
+class TokenTable(Base):
+    __tablename__ = 'token'
 
+    user_id = Column(Integer)
+    access_token = Column(String(100), primary_key=True)
+    refresh_token = Column(String(100), nullable=False)
+    status = Column(Boolean)
+    created_date = Column(DateTime, default=datetime.datetime.now)
